7884e383519da80a6fb1268700f14cfc3ba54f87
joong2043 <tbvjqlqlqlr@gmail.com>
2022-07-14 14:20:33 +0900

    명언 앱 시작 종료 실행 하여 반복문 멈추기

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class App {
 
+    void run(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---명언 SSG---");
+        outer:
+        while(true){
+            System.out.print("명령)");
+            String cmd = sc.nextLine();
+
+            switch (cmd) {
+                case "종료":
+                    break outer;
+            }
+
+        }
+
+        sc.close();
+    }
 }
