a8b83056ca048859060c0f037d8270877c5e8ed6
joong2043 <tbvjqlqlqlr@gmail.com>
2022-07-14 14:27:18 +0900

    등록 기능 구현(write) 후 id로 몇 번 명언 등록인지 구현

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
 public class App {
+    private Scanner sc;
 
+    public App(){
+        sc = new Scanner(System.in);
+    }
     void run(){
-        Scanner sc = new Scanner(System.in);
 
         System.out.println("---명언 SSG---");
         outer:
@@ -12,12 +15,25 @@ public class App {
             String cmd = sc.nextLine();
 
             switch (cmd) {
+                case "등록":
+                    write();
+                    break;
+
                 case "종료":
                     break outer;
             }
-
         }
-
         sc.close();
+    }
+
+    void write(){
+        int id = 1;
+        System.out.printf("명언 : ");
+        String content = sc.nextLine();
+
+        System.out.printf("작가 : ");
+        String author = sc.nextLine();
+
+        System.out.printf("%d번 명언이 등록 되었습니다.",id);
     }
 }
